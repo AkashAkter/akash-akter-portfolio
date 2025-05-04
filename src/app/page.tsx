eb0fd1c6f7080d79ea8e0c402afcebb42585bd20
AkashAkter <akash31akter@gmail.com>
2025-05-04 23:57:56 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,38 +13,24 @@ export default function Home() {
   return (
     <div>
       <Header />
-
-      {/* 1. Start with who you are */}
       <section id="about">
         <AboutMe />
       </section>
-
-      {/* 2. Show what you can do */}
       <section id="skills">
         <Skills />
       </section>
-
-      {/* 3. Demonstrate professional experience */}
       <section id="experience">
         <ExperienceSection />
       </section>
-
-      {/* 4. Showcase tangible projects */}
       <section id="projects">
         <ProjectSection />
       </section>
-
-      {/* 5. Highlight academic achievements */}
       <section id="education">
         <Educations />
       </section>
-
-      {/* 6. Display research work */}
       <section id="publications">
         <PublicationsSection />
       </section>
-
-      {/* 7. Show well-rounded personality */}
       <section id="activities">
         <ActivitiesSection />
       </section>
